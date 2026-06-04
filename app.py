a0efe5dd8763a57fbe17ad10a403e775155c0bc3
Fabio Giannasi <fabio.gianni24@gmail.com>
2026-06-04 15:49:52 +0200

    aggiunto middleware

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI #importo la classe da una libreria 
+from fastapi import FastAPI, Request #importo la classe da una libreria 
 from routes import router
+import time
 
 app = FastAPI() #costruttore di default della classe FastAPI 
 app.include_router(router)
+
+@app.middleware("http")
+async def measure_latency(request: Request, call_next):
+    start = time.time()
+    response = await call_next(request)
+    elapsed_ms = (time.time() - start) * 1000
+    print(f"Latency: {elapsed_ms:.2f} ms")
+    return response
 
 @app.get("/")
 def read_root():
