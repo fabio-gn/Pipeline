7953236219ac923ee8e120390d545ea47a86f429
Fabio Giannasi <fabio.gianni24@gmail.com>
2026-06-04 15:23:14 +0200

    aggiunto calcolo del tempo di risposta

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI #importo la classe da una libreria 
 from routes import router
 
-app = FastAPI()
+app = FastAPI() #costruttore di default della classe FastAPI 
 app.include_router(router)
 
 @app.get("/")
 def read_root():
     return{"bella"}
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
--- routes.py
+++ routes.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter, Query
 from area import calculate_triangle_area
+import time
 
 router = APIRouter()
 
+
+
 @router.get("/trianglearea/")
-def get_triangle_area(base: int, altezza: int) -> int:
-    return calculate_triangle_area(base, altezza)
+def get_triangle_area(base: int, altezza: int):
+    start = time.time()
+    area = calculate_triangle_area(base, altezza)
+    end = time.time()
+    elapsed = end - start
+    return {"area": area, "elapsed": elapsed}
